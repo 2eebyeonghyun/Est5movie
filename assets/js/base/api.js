573b319ec8e97396b1a8baee18b1aa1bde7eca93
2eebyeonghyun <wada0012@naver.com>
2025-01-21 09:16:54 +0900

    api.js에 깃 주소 등록작업

--- assets/js/base/api.js
+++ assets/js/base/api.js
@@ -1,6 +1,7 @@
 const api = {
     API_KEY: '2d7b9efb',
-    BASE_URL: 'http://www.omdbapi.com/'
+    BASE_URL: 'http://www.omdbapi.com/',
+    // GIT_URL: 'https://2eebyeonghyun.github.io/Est5movie/'
 }
 
 export default api;
