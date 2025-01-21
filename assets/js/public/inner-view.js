837cd82b9322bc2f41d82941ab86fd4c51b155da
kimsokyong53532585 <yong25850@gmail.com>
2025-01-21 17:44:01 +0900

    update:html.scss

--- assets/js/public/inner-view.js
+++ assets/js/public/inner-view.js
@@ -1,4 +1,4 @@
-import { darkMode } from '../components/dark-mode.js';
+import { darkMode } from "../components/dark-mode.js";
 
 // OMDb API 설정
 const API_KEY = "c2ca8aa";
@@ -13,27 +13,44 @@ async function fetchMovieDetails() {
   try {
     const response = await fetch(API_URL); // OMDb API 호출
     const movie = await response.json(); // JSON 데이터로 변환
-    console.log(movie);
-
     // 데이터를 HTML 구조로 렌더링
     movieContainer.innerHTML = `
-      <div class="movie-header">
-        <div class="movie-img">
-          <img src="${movie.Poster}" alt="${movie.Title} Poster" />
-        </div>
-        <div class="movie-detailsBox">
-          <div class="movie-title">${movie.Title}</div>
-          <a href="" class="btn-click" aria-label="예고편">WATCH</a>
-          <div class="movie-description">${movie.Plot} </div>
-          <div class="movie-informationBox">
-            <div class="movie-text movie-meta"><strong >평점</strong> ${movie.imdbRating}</div>
-            <div class="movie-text movie-yeaer"><strong>년도</strong> ${movie.Year}</div>
-            <div class="movie-text movie-meta"><strong>장르</strong> ${movie.Genre}</div>
-            <div class="movie-text movie-meta"><strong>배우</strong> ${movie.Actors}</div>
-            <div class="movie-text movie-meta"><strong>감독</strong> ${movie.Director}</div>
+       <div class="movie-header">
+          <div class="movie-img">
+            <img src="${movie.Poster}" alt="${movie.Title} Poster" />
+          </div>
+          <div class="movie-total-info">
+            <div class="movie-detail-info">
+              <div class="movie-detailsBox">
+                <div class="movie-title">${movie.Title}</div>
+                <a href="" class="btn-click" aria-label="예고편">WATCH</a>
+                <div class="movie-description">${movie.Plot}</div>
+                <div class="movie-informationBox">
+                  <div class="movie-text movie-meta"><strong>평점</strong> ${movie.imdbRating}</div>
+                  <div class="movie-text movie-yeaer"><strong>년도</strong> ${movie.Year}</div>
+                  <div class="movie-text movie-meta"><strong>장르</strong> ${movie.Genre}</div>
+                  <div class="movie-text movie-meta"><strong>배우</strong> ${movie.Actors}</div>
+                  <div class="movie-text movie-meta"><strong>감독</strong> ${movie.Director}</div>
+                </div>
+              </div>
+              <div class="movie-actors">
+                <div>
+                  <img src="${movie.Poser}" alt="${movie.Actors} Poster" />
+                </div>
+                <div>
+                  <img src="${movie.Poser}" alt="${movie.Actors} Poster" />
+                </div>
+              </div>
+            </div>
+            <div class="movie-info">
+              <div><img src="${movie.Poster}" alt="${movie.Actors} Poster" /></div>
+              <div><img src="${movie.Poster}" alt="${movie.Actors} Poster" /></div>
+              <div><img src="${movie.Poster}" alt="${movie.Actors} Poster" /></div>
+              <div><img src="${movie.Poster}" alt="${movie.Actors} Poster" /></div>
+              <div><img src="${movie.Poster}" alt="${movie.Actors} Poster" /></div>
+            </div>
           </div>
         </div>
-      </div>
      
     `;
   } catch (error) {
