26298f046e4f71f5e401ad514bb87add4e7f2fab
2eebyeonghyun <wada0012@naver.com>
2025-01-24 17:13:06 +0900

    update search.js

--- assets/js/components/search.js
+++ assets/js/components/search.js
@@ -92,7 +92,7 @@ export async function getMovies(value, year, type, page) {
             url += `&t=${type}`;
         }
 
-        const res = await fetch(url, {mode: 'cors'});
+        const res = await fetch(url, {mode: 'cors', cache: 'no-store'});
         const data = await res.json();
 
         if (data.Search) {
