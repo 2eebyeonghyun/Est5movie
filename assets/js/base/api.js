6874f6207868600be2c199802d5496a43433622b
2eebyeonghyun <wada0012@naver.com>
2025-01-24 15:42:45 +0900

    update 절대경로 수정

--- assets/js/base/api.js
+++ assets/js/base/api.js
@@ -1,7 +1,7 @@
 const api = {
     API_KEY: '2d7b9efb',
-    BASE_URL: 'http://www.omdbapi.com/',
-    GIT_URL: 'https://2eebyeonghyun.github.io/Est5movie/',
+    BASE_URL: 'https://www.omdbapi.com/',
+    GIT_URL: 'https://2eebyeonghyun.github.io/Est5movie',
     TMDB_KEY: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjJiMTFhZWYzZjg1YmQ1N2ViMTdlYTc2YjIxYTg5MyIsIm5iZiI6MTcwMjUzMTcxMS40MjcsInN1YiI6IjY1N2E5MjdmN2EzYzUyMDBlYmZlZmNiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Qlrj84jcEANg4pdpPQLeIYdKBipFGoUfLUAm44BGNLw'
 }
 
