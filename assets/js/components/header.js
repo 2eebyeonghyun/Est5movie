fb6967ee9eeeb2a7237b622a056b72cb61fe30a6
2eebyeonghyun <wada0012@naver.com>
2025-01-30 20:36:33 +0900

    250130 이병현 scrollHeader.js 수정 작업

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -8,7 +8,7 @@ export function scrollHeader() {
     // passive: true를 사용하면 preventDefault()를 사용할 수 없다.
     // 다만, 스크롤 성능이 개선이된다. 
     // 그래서 scroll 이벤트를 사용할 때 사용하면 좋다.
-    window.addEventListener("scroll", () => {
+    window.addEventListener('scroll', () => {
         const scrollY = window.scrollY;
 
         if (scrollY > lastScrollTop && scrollY > 100) {
@@ -17,18 +17,14 @@ export function scrollHeader() {
             headerWrap.style.transform = "translateY(0)";
         }
 
+        headerWrap.classList.toggle('scroll', scrollY > 50);
+
         lastScrollTop = scrollY;
     }, { passive: true });
 
-    window.addEventListener("mousemove", (e) => {
+    window.addEventListener('mousemove', (e) => {
         if (e.clientY <= 100) {
             headerWrap.style.transform = "translateY(0)";
         }
     }, { passive: true });
-
-    if (window.scrollY > 50) {
-        headerWrap.classList.add("scroll");
-    } else {
-        headerWrap.classList.remove("scroll");
-    }
 }
