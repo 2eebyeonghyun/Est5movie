489401418d314eb461781148f14f4f1613d2b7f5
2eebyeonghyun <wada0012@naver.com>
2025-01-31 15:15:51 +0900

    250131 이병현 scrollHeader 추가작업

--- assets/js/base/eventHandler.js
+++ assets/js/base/eventHandler.js
@@ -1,12 +1,14 @@
 import { get } from "./util.js";
 import { loadHeader } from "../components/loadHF.js";
 import { searchPoint } from "../components/search.js";
+import { scrollHeader } from "../components/scrollHeader.js";
 
 // 이벤트처리를 따로 분류했더니 중복이벤트오류가 발생하여 이벤트는 한 곳에서 호출
 export async function initializeEvents() {
 
     try {
         await loadHeader();
+        scrollHeader();
     
         // 서치 검색 영역
         const formEl = get(".form");
