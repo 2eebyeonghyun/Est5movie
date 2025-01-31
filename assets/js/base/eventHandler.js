7ceff36bfd3323617c274641c4714171ea974e34
2eebyeonghyun <wada0012@naver.com>
2025-01-31 18:54:51 +0900

    250131 이병현 이벤트핸들러 추가 작업

--- assets/js/base/eventHandler.js
+++ assets/js/base/eventHandler.js
@@ -9,25 +9,62 @@ export async function initializeEvents() {
         await loadHeader();
         scrollHeader();
     
-        document.addEventListener("click", (e) => {
-            if (e.target.matches(".btn-change")) {
-                let currentMode = document.documentElement.getAttribute("data-mode");
-                let newMode = currentMode === "dark" ? "light" : "dark";
-                try {
-                    localStorage.setItem("mode", newMode);
-                } catch (error) {
-                    console.error("localStorage 저장 실패", error);
-                }
-                document.documentElement.setAttribute("data-mode", newMode);
-            }
-        });
-
-        const formEl = document.querySelector(".form");
+        // 서치 검색 영역
+        const formEl = get(".form");
         if (formEl) {
             formEl.addEventListener("submit", (e) => {
                 e.preventDefault();
-                searchPoint(); // searchPoint 함수 호출
+                searchPoint();
             });
+        }
+
+        // 다크모드 영역
+        const button = get(".btn-change");
+        let theme;
+        try {
+            theme = localStorage.getItem('mode') || 'light';
+        } catch (error) {
+            console.error('localStorage 접근 실패', error);
+            theme = 'light';
+        }
+
+        let status = theme === 'dark';
+
+        if (status) {
+            themeDarkMode();
+        } else {
+            themeLightMode();
+        }
+
+        // 버튼 클릭 이벤트
+        button.addEventListener('click', () => {
+            if (status) {
+                themeLightMode();
+            } else {
+                themeDarkMode();
+            }
+        });
+
+        // 다크 모드 적용
+        function themeDarkMode() {
+            try {
+                localStorage.setItem("mode", "dark");
+            } catch (error) {
+                console.error('localStorage 저장 실패', error);
+            }
+            document.documentElement.setAttribute('data-mode', 'dark');
+            status = true;
+        }
+
+        // 라이트 모드 적용
+        function themeLightMode() {
+            try {
+                localStorage.setItem("mode", "light");
+            } catch (error) {
+                console.error('localStorage 저장 실패', error);
+            }
+            document.documentElement.setAttribute('data-mode', 'light');
+            status = false;
         }
     } catch (error) {
         console.error('error', error);
