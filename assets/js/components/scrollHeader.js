50efd38273dcd7ee1b6f2f291549fb637aa637e1
2eebyeonghyun <wada0012@naver.com>
2025-02-04 13:50:55 +0900

    250204 이병현 scrollHeader 주석 추가 작업

--- assets/js/components/scrollHeader.js
+++ assets/js/components/scrollHeader.js
@@ -9,8 +9,11 @@ export function scrollHeader() {
     // 다만, 스크롤 성능이 개선이된다. 
     // 그래서 scroll 이벤트를 사용할 때 사용하면 좋다.
     window.addEventListener('scroll', () => {
+        // 현재 scroll 위치 값을 가져온다.
         const scrollY = window.scrollY;
 
+        // 위치값보다 아래로 스크롤하거나 100px 스크롤 했을 경우 헤더를 y축으로 -100% 이동시킨다.
+        // 그게 아니면( 위로 올리면) 헤더가 원위치로 돌아온다.
         if (scrollY > lastScrollTop && scrollY > 100) {
             headerWrap.style.transform = "translateY(-100%)";
         } else {
