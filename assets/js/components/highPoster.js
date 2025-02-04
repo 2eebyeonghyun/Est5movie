9a094cb14264f85309cc357a6a1d994896425c1c
2eebyeonghyun <wada0012@naver.com>
2025-02-04 10:08:46 +0900

    250204 이병현 highPoster SX3000 -> SX2000 수정

--- assets/js/components/highPoster.js
+++ assets/js/components/highPoster.js
@@ -1,6 +1,6 @@
 export function getHighPoster(poster) {
     if (poster !== "N/A") {
-        return poster.replace("SX300", "SX3000");
+        return poster.replace("SX300", "SX2000");
     } else {
         return "/assets/images/poster-NotAvailable.png";
     }
